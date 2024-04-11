a385917538105e1c05bf076304a96d86d3f64547
warisz1 <warisah2665@gmail.com>
2024-04-11 10:03:27 +0200

    mockitem og billeder

---
+++ ZealandPetShop/MockData/MockItem.cs
@@ -0,0 +1,20 @@
+﻿using ZealandPetShop.Models.Shop;
+
+namespace ZealandPetShop.MockData
+{
+    public class MockItem
+    {
+        private static List<Item> _items = new List<Item>()
+        {
+            new Item("Kattebakke", "/images/greyLitterBox.png" ,"Grå platik kattebakke", 150.00),
+            new Item("Halsbånd", "/images/hundeHalsbånd.png" ,"Sort hunde halsbånd", 45.00),
+            new Item("Kaninbur", "/images/kaninburBlå.png" ,"Kaninbur med blå bund", 300.00),
+            new Item("Børste", "/images/grønBørste.png" ,"Grøn børste", 35.00),
+            new Item("Madskål", "/images/gulMadskål.png" ,"Gul madskål", 20.00)
+        };
+
+        public static List<Item> GetMockItems()
+        { return _items; }
+
+    }
+}
