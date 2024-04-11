7917b8e7d6bd2cacc9cafef6c7991514080079d2
warisz1 <warisah2665@gmail.com>
2024-04-11 11:15:17 +0200

    Udkommenteret error

--- ZealandPetShop/Models/Shop/Item.cs
+++ ZealandPetShop/Models/Shop/Item.cs
@@ -4,21 +4,21 @@
     {
         static private int NextId = 0;
 
-        [Display(Name = "Item ID")]
-        [Required(ErrorMessage = "Der skal angives et ID til Item")]
-        [Range(typeof(int), "0", "10000", ErrorMessage = "ID skal være mellem (1) og (2)")]
+        //[Display(Name = "Item ID")]
+        //[Required(ErrorMessage = "Der skal angives et ID til Item")]
+        //[Range(typeof(int), "0", "10000", ErrorMessage = "ID skal være mellem (1) og (2)")]
         public int? Id { get; set; }
 
         public string ImagePath { get; set; }
 
-        [Display(Name = "Item Navn")]
-        [Required(ErrorMessage = "Item skal have et navn")]
+        //[Display(Name = "Item Navn")]
+        //[Required(ErrorMessage = "Item skal have et navn")]
         public string? Name { get; set; }
 
         public string Description { get; set; }
 
-        [Display(Name = "Pris")]
-        [Required(ErrorMessage = "Der skal angives en pris")]
+        //[Display(Name = "Pris")]
+        //[Required(ErrorMessage = "Der skal angives en pris")]
         public double? Price { get; set; }
 
         public Item()
