17b3d8cad3a996a98686bc5d0bfcc600b2f93db3
Marjorine Pearl Zalwango <zalwango46@gmail.com>
2024-04-11 09:38:45 +0200

    item model

--- ZealandPetShop/Models/Shop/Item.cs
+++ ZealandPetShop/Models/Shop/Item.cs
@@ -2,5 +2,28 @@
 {
     public class Item
     {
+        [Display(Name = "Item ID")]
+        [Required(ErrorMessage = "Der skal angives et ID til Item")]
+        [Range(typeof(int), "0", "10000", ErrorMessage = "ID skal være mellem (1) og (2)")]
+        public int? Id { get; set; }
+
+        [Display(Name = "Item Navn")]
+        [Required(ErrorMessage = "Item skal have et navn")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Pris")]
+        [Required(ErrorMessage = "Der skal angives en pris")]
+        public double? Price { get; set; }
+
+        public Item()
+        {
+        }
+
+        public Item(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
     }
 }
