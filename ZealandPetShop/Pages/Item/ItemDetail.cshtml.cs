e762b45ae27dab850132b7fc4b6d5512cabb94cd
warisz1 <warisah2665@gmail.com>
2024-04-15 09:26:55 +0200

    ItemDetails page virker

---
+++ ZealandPetShop/Pages/Item/ItemDetail.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandPetShop.MockData;
+
+namespace ZealandPetShop.Pages.Item
+{
+    public class ItemDetailModel : PageModel
+    {
+
+        public List<Models.Shop.Item> Items { get; set; }
+
+        //public MockData.MockItem MockItem { get; set; }
+
+        public Models.Shop.Item Item { get; set; }
+
+
+
+        public Models.Shop.Item GetItemId(int id)
+        {
+            foreach (Models.Shop.Item item in Items)
+            {
+                if (item.Id == id)
+                { return item; }
+            }
+            return null;
+        }
+
+        public IActionResult OnGet(int Id)
+        {
+            Items = MockItem.GetMockItems();
+            Item = GetItemId(Id);
+            if (Item == null)
+            { return NotFound(); }
+            return Page();
+        }
+
+
+    }
+}
