99cac3ad6cb3dcab8bc6f4ceae1906453a409877
warisz1 <warisah2665@gmail.com>
2024-04-22 11:26:07 +0200

    orderrr

--- ZealandPetShop/Pages/Order/GetOrder.cshtml.cs
+++ ZealandPetShop/Pages/Order/GetOrder.cshtml.cs
@@ -1,23 +1,31 @@
 using ItemRazorV1.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandPetShop.MockData;
 
 namespace ZealandPetShop.Pages.Order
 {
     public class GetOrderModel : PageModel
     {
 
-        private ItemService _itemService;
+        //private ItemService _itemService;
 
         [BindProperty]
         public Models.Shop.Item Item { get; set; }
 
+        public List<Models.Shop.Item> Items { get; set; }
+
+        public void OnGet()
+        {
+            Items = MockItems.GetMockItems();
+        }
 
 
 
 
 
 
-       
+
+
     }
 }
