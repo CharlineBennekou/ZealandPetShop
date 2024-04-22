3194ed6cb31df4de702cea0cd073d13b743d7486
CharlineBennekou <149783592+CharlineBennekou@users.noreply.github.com>
2024-04-22 13:21:06 +0200

    .

--- ZealandPetShop/Pages/Item/ItemDetail.cshtml.cs
+++ ZealandPetShop/Pages/Item/ItemDetail.cshtml.cs
@@ -2,13 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using ZealandPetShop.MockData;
+using ZealandPetShop.Models.Shop;
 
 namespace ZealandPetShop.Pages.Item
 {
     public class ItemDetailModel : PageModel
     {
 
-
+        
         [BindProperty]
         [Required(ErrorMessage = "Indtast venligst mængden af produktet der ønskes")]
         public int Count { get; set; }
