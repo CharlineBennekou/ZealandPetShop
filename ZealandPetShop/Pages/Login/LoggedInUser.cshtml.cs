ba7026119a45171e073350a3607ff2cbcf648e7e
marjo0 <150661687+marjo0@users.noreply.github.com>
2024-05-02 09:23:15 +0200

    prifile

---
+++ ZealandPetShop/Pages/Login/LoggedInUser.cshtml.cs
@@ -0,0 +1,50 @@
+using ItemRazorV1.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+using ZealandPetShop.Models.Login;
+using ZealandPetShop.Services;
+
+namespace ZealandPetShop.Pages.Login
+{
+    public class LoggedInUserModel : PageModel
+    {
+
+
+
+        //public List<Models.Login.User> _user { get; set; }
+        //private UserService _userService;
+
+        //public IActionResult OnGet(string id)
+        //{
+           
+        //    int id = int.Parse(id);
+
+        //    return id;
+        //}
+        
+        
+        
+
+        
+
+        //public GetUser(UserService userService)
+        //{
+        //    _userService = userService;
+        //}
+
+
+        //public IActionResult OnGet(int id)
+        //{
+        //    Console.WriteLine("OnGet");
+        //    _user = _userService.GetUser(id);
+        //    if (_user == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Page();
+        //}
+
+     }
+}
