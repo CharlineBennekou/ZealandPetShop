18d76f32f85823d0a87e40603e0d3abdd50a52b4
marjo0 <150661687+marjo0@users.noreply.github.com>
2024-05-23 21:15:06 +0200

    update

---
+++ ZealandPetShop/Pages/Login/UpdateUser.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using ZealandPetShop.Models.Login;
+using ZealandPetShop.Services;
+
+namespace ZealandPetShop.Pages.Login
+{
+    public class UpdateUserModel : PageModel
+    {
+        private UserService _userService;
+
+        [BindProperty] 
+        public User EUser { get; set; }
+
+
+        [BindProperty, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        public UpdateUserModel(UserService userService)
+        {
+            _userService = userService;
+        }
+
+       public async Task <IActionResult> OnGetAsync()
+        {
+           
+        }
+    }
+}
