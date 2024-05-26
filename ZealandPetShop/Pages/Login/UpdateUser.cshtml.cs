5fb15743b7a4622187fa58461778129e9f3c368b
marjo0 <150661687+marjo0@users.noreply.github.com>
2024-05-26 21:32:20 +0200

    dlwbw

--- ZealandPetShop/Pages/Login/UpdateUser.cshtml.cs
+++ ZealandPetShop/Pages/Login/UpdateUser.cshtml.cs
@@ -1,38 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
+using System.Reflection;
+using System.Security.Claims; // Giver adgang til brugerens claims
 using ZealandPetShop.Models.Login;
 using ZealandPetShop.Services;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ZealandPetShop.Pages.Login
 {
     public class UpdateUserModel : PageModel
     {
+        //Diffinere en privat variabel til UserService
         private UserService _userService;
 
+        //Binder brugerdata til UI
         [BindProperty]
         public User EUser { get; set; }
 
-
+        // Binder password data til UI med en datavalidering
         [BindProperty, DataType(DataType.Password)]
         public string Password { get; set; }
 
-
+        // Konstruktor for at initialisere UserService
         public UpdateUserModel(UserService userService)
         {
             _userService = userService;
         }
 
+        // Metode, der kaldes ved GET-forespøregsler
         public async Task<IActionResult> OnGetAsync(int id)
         {
+            // Henter bruger-ID fra krav (claims)
             int userId = GetUserIdFromClaims();
+            // Henter brugerdata asynkront ved hjælp af UserService
             EUser = await _userService.GetUser(id);
+            //Hvis brugeren ikke findes, returner NotFound
             if (User == null) return NotFound();
 
+            // Returnerer Page visning
             return Page();
         }
 
+        // Metode, der kaldes ved POST-forespørgelser
         public async Task<IActionResult> OnPostAsync()
         {
             //if (!ModelState.IsValid) 
@@ -40,8 +50,10 @@ namespace ZealandPetShop.Pages.Login
             //return Page();
             //}
 
+            // Opdaterer brugerdata asynkront ved hjælp af UserService
             await _userService.UpdateUser(EUser);
 
+            // Returnerer Page visning
             return Page();
 
 
@@ -49,14 +61,18 @@ namespace ZealandPetShop.Pages.Login
         }
 
 
-
+        // Privat metode til at hente bruger-ID fra krav (claims)
         private int GetUserIdFromClaims()
         {
+            // Henter brugerens claims identity (som er Id)
             var claimsIdentity = User.Identity as ClaimsIdentity;
+            // Henter bruger-ID claim
             var userIdClaim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
 
+            // Hvis bruger-ID claim er null, kaster en undtagelse
             if (userIdClaim == null)
             {
+                // Forsøger at parse bruger-ID til en int
                 throw new InvalidOperationException("No claim found for user ID.");
             }
 
@@ -64,7 +80,7 @@ namespace ZealandPetShop.Pages.Login
             {
                 throw new InvalidOperationException("Invalid user ID claim.");
             }
-
+            // Returnerer bruger-ID
             return userId;
         }
 
