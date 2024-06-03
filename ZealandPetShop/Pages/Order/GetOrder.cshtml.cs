3a4565260b2ce1e22d6a3479da4995269bf27576
CharlineBennekou <149783592+CharlineBennekou@users.noreply.github.com>
2024-06-03 12:47:14 +0200

    Orders kan bestilles

--- ZealandPetShop/Pages/Order/GetOrder.cshtml.cs
+++ ZealandPetShop/Pages/Order/GetOrder.cshtml.cs
@@ -7,6 +7,8 @@ using ZealandPetShop.Models.Shop;
 using Microsoft.Extensions.Configuration.UserSecrets;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using ZealandPetShop.Models.Order;
+using ZealandPetShop.Models.Login;
 
 namespace ZealandPetShop.Pages.Order
 {
@@ -15,15 +17,18 @@ namespace ZealandPetShop.Pages.Order
         private readonly IHttpContextAccessor _httpContextAccessor; //bruges til at få fat i userId
         private ItemService _itemService; //bruges til at få fat i alle items
         private CartService _cartService; //bruges til at få fat i kundens orderitems i cart
+        private OrderService _orderService; //Bruges til at få fat i kundens order
         public List<Models.Order.OrderItem> OrderItems {  get; set; }
        
         public List<Models.Shop.Item> Items { get; set; }
+        public Models.Order.Order Order { get; set; }
 
-        public CartModel(CartService cartService, ItemService itemService, IHttpContextAccessor httpContextAccessor)
+        public CartModel(CartService cartService, ItemService itemService, IHttpContextAccessor httpContextAccessor, OrderService orderService)
         {
             _cartService = cartService;
             _itemService = itemService;
             _httpContextAccessor = httpContextAccessor;
+            _orderService = orderService;
         }
 
         public async Task OnGet()
@@ -31,9 +36,36 @@ namespace ZealandPetShop.Pages.Order
             //Finder logget ind bruger's userid
             int userid = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
             Items = _itemService.GetItems(); //Henter alle items
-            OrderItems = await _cartService.GetCartItemsByUserId(userid); //henter alle cart items vha. userid
+            Order = _orderService.GetOrderCartByUserIdAsync(userid).Result;
+            OrderItems = _cartService.GetOrderItemsByOrderIdAsync(Order.Id).Result;
+
 
         }
+
+        public async Task<IActionResult> OnPostDeleteOrderItem(int id)
+        {
+           await _cartService.DeleteOrderItemAsync(id);
+
+            //Vi bliver nødt til at assign vores Items, order og orderitems med data før vi redirecter, da der ellers kommer null exception.
+            int userid = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            Items = _itemService.GetItems(); //Henter alle items
+            Order = _orderService.GetOrderCartByUserIdAsync(userid).Result;
+            OrderItems = _cartService.GetOrderItemsByOrderIdAsync(Order.Id).Result;
+            return Page();
+
+        }
+        public async Task<IActionResult> OnPostBestil(int id)
+        {
+            _orderService.MarkAsOrdered(id);
+
+            //Vi bliver nødt til at assign vores Items, order og orderitems med data før vi redirecter, da der ellers kommer null exception.
+            int userid = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            //Items = _itemService.GetItems(); //Henter alle items
+            //Order = _orderService.GetOrderCartByUserIdAsync(userid).Result;
+            //OrderItems = _cartService.GetOrderItemsByOrderIdAsync(Order.Id).Result;
+            return Page();
+        }
+ 
 
         public Models.Shop.Item GetItemById(int itemId) //_itemService.GetItem(); er privat, så vi laver den her metode for at bruge den i html
         {
