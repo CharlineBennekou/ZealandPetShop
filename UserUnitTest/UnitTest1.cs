2a0da78bca84ac55f50b7f2ecc5397965eaf02bc
CharlineBennekou <charline@bennekou.eu>
2024-06-08 11:17:34 +0200

    Unittest

--- UserUnitTest/UnitTest1.cs
+++ UserUnitTest/UnitTest1.cs
@@ -40,6 +40,7 @@ namespace UserUnitTesting
         {
             // Act & Assert
             Assert.ThrowsException<ArgumentException>(() => _user.Email = "invalid-email");
+            Assert.ThrowsException<ArgumentException>(() => _user.Email = "admin");
             _user.Email = "email@gmail.com";
             Assert.AreEqual("email@gmail.com", _user.Email);
         }
